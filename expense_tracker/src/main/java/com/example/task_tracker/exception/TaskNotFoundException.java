a912b877816d5c090d0f4e089b9f65cebb159f66
ductm <macos@MacOss-MacBook-Air.local>
2025-11-18 10:00:02 +0700

    first commit

--- expense_tracker/src/main/java/com/example/task_tracker/exception/TaskNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.task_tracker.exception;
-
-public class TaskNotFoundException extends RuntimeException {
-    public TaskNotFoundException(String message) {
-        super(message);
-    }
-}
--- expense_tracker/src/main/java/com/example/task_tracker/exception/TaskOperationException.java
+++
@@ -1,7 +0,0 @@
-package com.example.task_tracker.exception;
-
-public class TaskOperationException extends RuntimeException {
-    public TaskOperationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
